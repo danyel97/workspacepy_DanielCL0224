ef140bade1e38e849741f1fb2470c57e3d48d947
danyel97 <158854843+danyel97@users.noreply.github.com>
2024-02-13 13:17:11 -0500

    Update ejer_2.py

--- modulo1/practica/ejer_2.py
+++ modulo1/practica/ejer_2.py
@@ -1,1 +1,22 @@
+print("****Calculadora de Areas****")
+fig = input("Ingrese la figura deseada: ")
 
+if fig == "circulo" or "cuadrado" or "triangulo":
+    import math
+    if fig == "cuadrado":
+        lado = float(input("Ingrese el lado del cuadrado: "))
+        area2 = math.pow(lado,2)
+        print(area2)
+    elif fig == "triangulo":
+        base = float(input("Ingrese la base del triangulo: "))
+        alt = float(input("Ingrese la altura del triangulo: "))
+        area3 = (base*alt)/2
+        print(area3)
+    elif fig == "circulo":
+         PI = math.pi # float
+         rad = float(input("Ingrese el radio del circulo: "))
+         area=PI*math.pow(rad,2)
+         print(area)
+else:
+    msg = "no es la figura"
+    print(msg)
